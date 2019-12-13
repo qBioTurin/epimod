1114c0974f59c45c91bc445756e7d35667cb7dce
PerniceSimone <PerniceSimone@github.com>
2019-12-13 16:47:40 +0100

    SP: updating the downloadContainers' documentation

--- R/downloadContainers.R
+++ R/downloadContainers.R
@@ -1,13 +1,12 @@
 #' @title Download for the first time all containers embedded in the workflows
-#' @description This is a functin that preapre the docker environment to be used for the first time the docker4seq is installed.
-#' @param group, a character string. Two options: \code{"sudo"} or \code{"docker"}, depending to which group the user belongs
-#' @param containers.file, a character string with the name of the file which indicate which are the initial set of containers to be downloaded. Initally the set is given by a file located in the folder containers of docker4seq package: "all", "rnaseq", "ncrnaseq", "chipseq"
-#' @author Raffaele Calogero
+#' @description This is a function that prepares the docker environment to be used for the first time the epimod is installed.
+#' @param containers.file, a character string with the name of the file which indicate which are the initial set of containers to be downloaded. If NULL then the set is given by a file called "containersNames.txt" located in the folder inst/Containers of epimod package.
+#' @author Marco Beccuti
 #'
 #' @examples
 #'\dontrun{
 ##'     #running runDocker
-#'      downloadContainers(group="docker", containers.file)
+#'      downloadContainers()
 #'
 #' }
 #' @export
