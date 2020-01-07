6931ca2b95dd7429acc439b35576cf6829952490
PerniceSimone <PerniceSimone@github.com>
2020-01-07 14:43:38 +0100

    example functions

--- Example/Functions.R
+++ Example/Functions.R
@@ -3,8 +3,14 @@
 #
 ###
 
-init_predator_generation<-function(min , max )
+init_generation<-function(min_init , max_init)
 {
-   return(runif(n=1,min=min,max=max))
+cat(min_init)
+   # min/max are vectors = first position interval values for the first place and second position for the second place
+
+   i_1=runif(n=1,min=min_init[1],max=max_init[1])
+   i_2=runif(n=1,min=min_init[2],max=max_init[2])
+
+   return( c(i_1,i_2) )
 }
 
