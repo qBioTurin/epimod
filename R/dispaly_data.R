6759db132aa52f573efb7fdc6a07b9ccdd86fc0a
Paolo Castagno <castagno@di.unito.it>
2020-03-10 11:27:26 +0100

    Introducing display function

--- R/dispaly_data.R
+++ R/dispaly_data.R
@@ -14,20 +14,31 @@
 #'               port = getOption("shiny.port"))
 #' }
 #' @export
-sensitivity_analysis <-function(
+display_data <-function(
 	volume = getwd(),
+	ip = "192.168.1.1",
 	port = getOption("shiny.port"))
 {
+	chk_dir<- function(path){
+		pwd <- basename(path)
+		return(paste0(file.path(dirname(path),pwd, fsep = .Platform$file.sep), .Platform$file.sep))
+	}
+
 	volume <- tools::file_path_as_absolute(volume)
-	parmas <- list(port = port,
-				   dir = "/home/docker/data/")
+	# parms <- list(port = port,
+	# 			   dir = "/home/docker/data/")
 	# Save all the parameters to file, in a location accessible from inside the dockerized environment
-	p_fname <- paste0(res_dir, parms_fname,".RDS")
+	# p_fname <- paste0(chk_dir(volume), "display_data.RDS")
 	# Use version = 2 for compatibility issue
-	saveRDS(parms,  file = p_fname, version = 2)
-	p_fname <- paste0( parms$out_dir, parms_fname,".RDS") # location on the docker image file system
+	# saveRDS(parms,  file = p_fname, version = 2)
+	# p_fname <- paste0( parms$out_dir, "display_data.RDS") # location on the docker image file system
 	# Run the docker image
 	containers.file=paste(path.package(package="epimod"),"Containers/containersNames.txt",sep="/")
 	containers.names=read.table(containers.file,header=T,stringsAsFactors = F)
-	docker.run(params = paste0("--cidfile=dockerID ","--volume ", volume,":", dirname(parms$dir), " -d ", containers.names["display",1]," Rscript /usr/local/lib/R/site-library/epimod/R_scripts/display.mngr.R ", p_fname))
+	# docker.run(params = paste0("--cidfile=dockerID ","--volume ", volume,":", dirname(parms$dir), " -d ", containers.names["display",1]," Rscript /usr/local/lib/R/site-library/epimod/R_scripts/display.mngr.R ", p_fname))
+	docker.run(params = paste0("--cidfile=dockerID ",
+							   "--volume ", volume,":", dirname(parms$dir),
+							   " -p ", ip, ":",port,":8080",
+							   " -d ", containers.names["display",1],
+							   " Rscript /usr/local/lib/R/site-library/epimod/R_scripts/display.mngr.R"))
 }
--- inst/R_scripts/display.mngr.R
+++ inst/R_scripts/display.mngr.R
@@ -1,18 +1,16 @@
-library(parallel)
 library(epimod)
 
-chk_dir<- function(path){
-	pwd <- basename(path)
-	return(paste0(file.path(dirname(path),pwd, fsep = .Platform$file.sep), .Platform$file.sep))
-}
+# chk_dir<- function(path){
+# 	pwd <- basename(path)
+# 	return(paste0(file.path(dirname(path),pwd, fsep = .Platform$file.sep), .Platform$file.sep))
+# }
 # Read commandline arguments
-args <- commandArgs(TRUE)
-cat(args)
-param_fname <- args[1]
+# args <- commandArgs(TRUE)
+# cat(args)
+# param_fname <- args[1]
 # Load parameters
-params <- readRDS(param_fname)
+# params <- readRDS(param_fname)
 # Load seed and previous configuration, if required.
-setwd(params$dir)
-runAopp(appDir = getwd(),
-		port = params$portm
-		)
+runApp(appDir = "~/data",
+	   port = 8080
+	   )
