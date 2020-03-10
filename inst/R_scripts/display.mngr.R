51ceb120ce9c7452adc09d0b1c071bc44d4a26d5
Paolo Castagno <castagno@di.unito.it>
2020-03-10 15:38:08 +0100

    Introducing display function

--- inst/R_scripts/display.mngr.R
+++ inst/R_scripts/display.mngr.R
@@ -1,16 +1,6 @@
 library(epimod)
 library(shiny)
-# chk_dir<- function(path){
-# 	pwd <- basename(path)
-# 	return(paste0(file.path(dirname(path),pwd, fsep = .Platform$file.sep), .Platform$file.sep))
-# }
-# Read commandline arguments
-# args <- commandArgs(TRUE)
-# cat(args)
-# param_fname <- args[1]
-# Load parameters
-# params <- readRDS(param_fname)
-# Load seed and previous configuration, if required.
-runApp(appDir = "~/data",
+
+runApp(appDir = "~/",
 	   port = 8080
 	   )
