1bb932874a8a07137d21aeff8d2c3b0249e02eee
lucarosso46 <luca.rosso46@edu.unito.it>
2021-04-19 22:45:49 +0200

    Added new control for existence of file

--- R/common_test.R
+++ R/common_test.R
@@ -7,7 +7,7 @@ common_test<-function(net_fname,functions_fname = NULL,reference_data = NULL,tar
 
 
   if(!missing(functions_fname)){
-    if(!is.null(functions_fname) & !file.exists(functions_fname)){
+    if(is.null(functions_fname) | !file.exists(functions_fname)){
       suggested_files = list.files(path = getwd(),
                                    pattern = ifelse(caller_function=="generation","*.cpp$","*.R$"),
                                    recursive = TRUE)
@@ -25,7 +25,7 @@ common_test<-function(net_fname,functions_fname = NULL,reference_data = NULL,tar
     if(missing(net_fname))
       return("net_fname parameter is missing! Abort")
     else{
-      if(!is.null(net_fname) & !file.exists(net_fname)){
+      if(is.null(net_fname) | !file.exists(net_fname)){
         pnpro_files = list.files(path = getwd(), pattern = "*.PNPRO$",recursive = TRUE)
         return(paste("File",net_fname,"of net_fname parameter not exists, list of .PNPRO files found:\n\t",
                    paste(unlist(pnpro_files),collapse = "\n\t")))
@@ -47,13 +47,13 @@ common_test<-function(net_fname,functions_fname = NULL,reference_data = NULL,tar
 
 
     if(!missing(reference_data)){
-      if(!is.null(reference_data) & !file.exists(reference_data)){
+      if(is.null(reference_data) | !file.exists(reference_data)){
         R_files = list.files(path = getwd(), pattern = "*.csv$",recursive = TRUE)
         return(paste("File",reference_data,"of reference_data parameter not exists,",
                      "list of .csv files found:\n\t",paste(unlist(R_files),collapse = "\n\t")))
       }else{
         if(!missing(distance_measure_fname)){
-          if(!is.null(distance_measure_fname) & !file.exists(distance_measure_fname)){
+          if(is.null(distance_measure_fname) | !file.exists(distance_measure_fname)){
             R_files = list.files(path = getwd(), pattern = "*.R$",recursive = TRUE)
             return(paste("File",distance_measure_fname,"of distance_measure_fname parameter not exists,",
                          "list of .R files found:\n\t",paste(unlist(R_files),collapse = "\n\t")))
@@ -79,7 +79,7 @@ common_test<-function(net_fname,functions_fname = NULL,reference_data = NULL,tar
 
 
     if(!missing(target_value_fname)){
-      if(!is.null(target_value_fname) & !file.exists(target_value_fname)){
+      if(is.null(target_value_fname) | !file.exists(target_value_fname)){
         R_files = list.files(path = getwd(), pattern = "*.R$",recursive = TRUE)
         return(paste("File",target_value_fname,"of target_value_fname parameter not exists,",
                    "list of .R files found:\n\t",paste(unlist(R_files),collapse = "\n\t")))
@@ -143,7 +143,7 @@ common_test<-function(net_fname,functions_fname = NULL,reference_data = NULL,tar
     if(missing(solver_fname))
       return("solver_fname parameter is missing! Abort")
     else{
-      if(!is.null(solver_fname) & !file.exists(solver_fname)){
+      if(is.null(solver_fname) | !file.exists(solver_fname)){
         solver_files = list.files(path = getwd(), pattern = "*.solver$",recursive = TRUE)
         return(paste("File",solver_fname,"of solver_fname parameter not exists, list of .solver files found:\n\t",
                      paste(unlist(solver_files),collapse = "\n\t")))
@@ -165,7 +165,7 @@ common_test<-function(net_fname,functions_fname = NULL,reference_data = NULL,tar
 
     #if not specified, a runtime error is generated
     if(!missing(parameters_fname)){
-      if(!is.null(parameters_fname) & !file.exists(parameters_fname)){
+      if(is.null(parameters_fname) | !file.exists(parameters_fname)){
         return(paste("File", parameters_fname, "of parameters_fname parameter does not exist!"))
       }
       else{
