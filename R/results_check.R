ef7a80a503ee8a7319cca23a5d5d35b19d8e4d5c
lucarosso46 <luca.rosso46@edu.unito.it>
2021-09-07 13:27:15 +0200

    Trying to plot single spline of SSA and TAUG solvers

--- R/results_check.R
+++ R/results_check.R
@@ -387,8 +387,8 @@ fda_check<-function(fname_st=NULL,fname_nd=NULL,furl_st=NULL, sep=" ", threshold
 		print(ncol(trace1.ready))
 		for(i in c(1:(ncol(trace1.ready))))
 		{
-			plot(i,paste0(trace1.ready$,"Time",i-1),type="l",col="blue")
-			points(i,paste0(trace1.ready$,"Time",i-1),type="l",col="blue")
+			plot(c(1:ncol(trace1.ready)),paste0(trace1.ready$,"Time",i-1),type="l",col="blue")
+			points(c(1:ncol(trace1.ready)),paste0(trace1.ready$,"Time",i-1),type="l",col="blue")
 		}
 
 		#Open graphic device to print plot and images on png
