7bb3b4adf346d462c03e0234d52d542e38ab6b8d
lucarosso46 <luca.rosso46@edu.unito.it>
2021-09-09 16:02:30 +0200

    Add build_images.sh file

---
+++ inst/Docker/build_images.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+IMAGES=$(ls -d */)
+BASEDIR=$(pwd) 
+if [ $# -ne 1 ]; then
+	echo "Illegal number of parameters"
+	echo "Usage:"
+	echo "\tbuild_images TAGNAME"
+	echo "where TAGNAME will be used to tag the images uploaded to Docker Hub."
+	exit 1
+else
+	TAG=$1
+fi
+for I in $IMAGES; do
+	# Make all letters lowercase and remove the ending babcslash
+	IMG=$(echo $I | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]')
+	DIR=$BASEDIR/$I
+	cd $DIR
+	if [ -f $DIR/Dockerfile ]; then
+		date > marker
+		echo "Building $IMG"
+		echo -e "\tExecuting docker build -t qbioturin/epimod-$IMG:$TAG ."
+		docker build -t qbioturin/epimod-$IMG:$TAG .
+		if [[ $? -ne 0 ]]; then
+			exit 0
+		fi	
+		rm marker
+		echo "Uploading $IMG"
+	       	echo -e "\tExecuting docker push qbioturin/epimod-$IMG:$TAG"
+		docker push qbioturin/epimod-$IMG:$TAG
+		if [[ $? -ne 0 ]]; then
+			exit 0
+		fi	
+	else
+		echo "Dockerfile missing for image qbioturin/epimod-$IMG"
+	fi
+	cd $BASEDIR
+done
+exit 0
