93115376e94be8aa95beb9d4a38a865c1ddf17ef
Paolo Castagno <castagno@di.unito.it>
2021-09-09 15:12:39 +0200

    Fixing stopped container removal

--- R/docker_run.R
+++ R/docker_run.R
@@ -74,16 +74,18 @@ docker.run <- function( params=NULL, changeUID=TRUE, debug=FALSE){
     #     return(3)
     # }
 
-    if(debug==TRUE){ #as.numeric(dockerExit)!=0
+    if(debug==TRUE){
     	system(paste("docker logs ", substr(dockerid,1,12), " &> ", substr(dockerid,1,12),"_error.log", sep=""))
+    	cat("The container's log is saved at: ")
+    	system(paste0("docker inspect --format=","'{{.LogPath}}' ",dockerid))
+    	system("echo 0 > ExitStatusFile 2>&1")
+    	return(0)
+    }
+    if(as.numeric(dockerExit)!=0){
 	    cat(paste("\nDocker container ", substr(dockerid,1,12), " had exit different from 0\n", sep=""))
 	    cat("\nExecution is interrupted\n")
 	    cat(paste("Please send to beccuti@unito.it this error: Docker failed exit 0,\n the description of the function you were using and the following error log file,\n which is saved in your working folder:\n", substr(dockerid,1,12),"_error.log\n", sep=""))
 	    system("echo 3 > ExitStatusFile 2>&1")
-
-
-	    cat("The container's log is saved at: ")
-	    system(paste0("docker inspect --format=","'{{.LogPath}}' ",dockerid))
 	    return(3)
     }
 
