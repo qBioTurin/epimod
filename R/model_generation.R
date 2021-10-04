44edda827a3a59d0be21fd3f9774fc8394884acc
paolocastagno <34031705+paolocastagno@users.noreply.github.com>
2021-10-04 12:21:51 +0200

    Update function description

--- R/model_generation.R
+++ R/model_generation.R
@@ -1,32 +1,17 @@
-#' @title Run model calibration
-#' @description this functon takes as input a solver and all the required parameters to set up a dockerized running environment to perform model calibration. both for deterministic and stochastic models
-#' In order to run the simulations, the user must provide a reference dataset and the definition of a function to compute the distance (or error) between the models' output and the reference dataset itself.
-#' The function defining the distance has to be in the following form:
+#' @title Run model generation
+#' @description 
 #'
-#' FUNCTION_NAME(reference_dataset, siulation_output)
+#'      Having constructed the model exploiting the graphical editor, namely GreatSPN, the automatic generation of both the stochastic (the Continuous Time Markov Chain) and
+#'      deterministic (ODEs) processes underlying the model is implemented by the model_generation() function.
 #'
-#' Moreover, the function must return a column vector with one entry for each evaluation point (i.e. f_time/s_time entries)
-#' in addiction to that, the user is asked to provide a function that, given the output of the solver, returns the releveant measure (one column) used to evalaute the quality of the solution.
-#'
-#' The sensitivity analysis will be performed through a Monte Carlo sampling throug user defined functions.
-#' the parameters involved in the sensitivity analysis have to be listed in a cvs file using the following structure:
-#'
-#' OUTPUT_FILE_NAME, FUNCTION_NAME, LIST OF PARAMETERS (comma separated)
-#'
-#' The functions allowed to compute the parameters are either R functions or user defined functions. In the latter case, all the user defined functions must be provided in a single .R file (which will be passed to run_sensitivity through the parameter parameters_fname)
-#'
-#' Exploiting the same mechanism, user can provide an initial marking to the solver. However, if it is the case the corresponding file name in the parameter list must be set to "init"
-#'
-#' To drive the optimization, the user has to provide a function to generate a new configuration, starting from a vector of n elements (each one ranging from 0 to 1).
-#' Furthermore, the vector init_v defines the initial point of the search.
-#'
-#' IMPORTANT: the length of the vector init_v defines the number of variables to variate within the search of the optimal configuration.
-#'
-#' @param out_fname Prefix to the output file name
-#' @param net_fname .PNPRO file storing  the model as ESPN. In case there are multiple nets defined within the PNPRO file, the first one in the list is the will be automatically selected;
-#' @param functions_fname  C++ file defining the functions managing the behaviour of general transitions.
+#' @param out_fname Prefix to the output file name.
+#' @param net_fname .PNPRO file storing  the Petri Net (and all its generalizations) model. In case there are multiple nets defined within the PNPRO file, the first one in the list is the will be automatically selected.
+#' @param functions_fname  C++ file defining the functions managing the behaviour of general transitions, mandatory if Extended versions of Petri Nets (i.e., ESPN or ESSN) are used.
+#' @param volume The folder to mount within the Doker image providing all the necessary files.
+
 #' @author Beccuti Marco, Castagno Paolo, Pernice Simone
 
+#' @return Model_generation returns the binary file SIR.solver in which the underlying processes (both deterministic and stochastic) of the Petri Net model and the library used for their simulation are packaged.
 #'
 #' @examples
 #'\dontrun{
@@ -35,6 +20,10 @@
 #'                  net_fname = paste0(local_dir, "Configuration/Pertussis"),
 #'                  functions_fname = "transitions.cpp")
 #' }
+#'
+#' @details 
+#'    GreatSPN GUI, the graphical editor for drawing Petri Nets formalisms, is available online: http://www.di.unito.it/~amparore/mc4cslta/editor.html
+#'    
 #' @export
 model_generation <-function( out_fname = NULL,
                              net_fname,
@@ -103,6 +92,7 @@ model_generation <-function( out_fname = NULL,
         file.copy(file.path(out_dir,paste0(basename(netname), ".solver"),fsep = .Platform$file.sep),chk_dir(volume))
         file.copy(file.path(out_dir,paste0(basename(netname), ".net"),fsep = .Platform$file.sep),chk_dir(volume))
         file.copy(file.path(out_dir,paste0(basename(netname), ".def"),fsep = .Platform$file.sep),chk_dir(volume))
+        file.copy(file.path(out_dir,paste0(netname, ".PlaceTransition"),fsep = .Platform$file.sep),chk_dir(volume))
         unlink(out_dir, recursive = TRUE)
     }
 
