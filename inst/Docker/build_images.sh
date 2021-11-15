74d3a46cee3030f98332fd15ff67e90223f1d38e
Daniele Baccega <daniele.baccega@unito.it>
2021-11-15 14:18:13 +0100

    Update seed management

--- inst/Docker/build_images.sh
+++ inst/Docker/build_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-IMAGES=$(ls -d */)
+#IMAGES=$(ls -d */)
+IMAGES=(Analysis Calibration Sensitivity)
 BASEDIR=$(pwd) 
 if [ $# -ne 1 ]; then
 	echo "Illegal number of parameters"
@@ -10,7 +11,7 @@ if [ $# -ne 1 ]; then
 else
 	TAG=$1
 fi
-for I in $IMAGES; do
+for I in ${IMAGES[@]}; do
 	# Make all letters lowercase and remove the ending babcslash
 	IMG=$(echo $I | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]')
 	DIR=$BASEDIR/$I
@@ -26,7 +27,7 @@ for I in $IMAGES; do
 		rm marker
 		echo "Uploading $IMG"
 	       	echo -e "\tExecuting docker push qbioturin/epimod-$IMG:$TAG"
-		docker push qbioturin/epimod-$IMG:$TAG
+		#docker push qbioturin/epimod-$IMG:$TAG
 		if [[ $? -ne 0 ]]; then
 			exit 0
 		fi	
