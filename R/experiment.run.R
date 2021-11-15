0ddbcaef7ffe5e1954fedb2d4c0359f2e0acf345
Paolo Castagno <castagno@di.unito.it>
2021-11-15 14:56:15 +0100

    Fixing discrete events capability

--- R/experiment.run.R
+++ R/experiment.run.R
@@ -117,19 +117,21 @@ worker <- function(worker_id,
 			file.rename(from = curr_fnm, to = fnm)
 			### DEBUG ###
 			# system(paste0("cp ", fnm, " ~/data/", fnm))
+			### DEBUG ###
 		}
 		else{
-			# DEBUG
+			### DEBUG ###
 			# system(paste0("cp ", curr_fnm, " ~/data/", curr_fnm))
-			# DEBUG
-			# write(x = paste0("tail -n $(expr $(wc -l ", curr_fnm, " | cut -f1 -d' ') - 2) ", curr_fnm, " >> ", fnm), file = "~/data/commands.txt", append = TRUE)
-			#system(paste0("tail -n $(expr $(wc -l ", curr_fnm, " | cut -f1 -d' ') - 2) ", curr_fnm, " >> ", fnm))
-			#system(paste0("tail -n $(expr $(wc -l ", curr_fnm, " | cut -f1 -d' ') - 2) ", curr_fnm, " >> ", fnm))
+			### DEBUG ###
 			# Remove last line from the output file
+			### DEBUG ###
 			print(paste0("head -n-1 ", fnm))
+			### DEBUG ###
 			system(paste0("head -n-1 ", fnm, " > ", fnm))
 			# Remove first line from the current output file and append to the output file
+			### DEBUG ###
 			print(paste0("tail -n-$(($(wc -l ", curr_fnm, " | cut -f1 -d' ') - 1)) ", curr_fnm))
+			### DEBUG ###
 			system(paste0("tail -n-$(($(wc -l ", curr_fnm, " | cut -f1 -d' ') - 1)) ", curr_fnm, " >> ", fnm))
 			file.remove(curr_fnm)
 
