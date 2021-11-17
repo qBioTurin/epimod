b9208e3e0659c3a094ec35f0ed04155890173372
Daniele Baccega <daniele.baccega@unito.it>
2021-11-17 18:04:42 +0100

    Try to use the same directory with extended sensitivity analysis

--- R/experiment_configuration.R
+++ R/experiment_configuration.R
@@ -72,7 +72,8 @@ experiment.configurations <- function(n_config,
             }
         }
         #for(j in c(1:n_config)){
-        for(j in c(length(config)+1:length(config)+n_config)){
+        config_length <- length(config)
+        for(j in c(config_length+1:config_length+n_config)){
             if(j==1)
                 config[[i]] <- list()
             if(!is.null(ini_vector) && is_function && "x" %in% formalArgs(f)){
