7795d3f8fa5622bdca76444029ee035013a2f7e9
Daniele Baccega <daniele.baccega@unito.it>
2021-11-19 14:33:23 +0100

    Seed management in model_calibration and model_analysis

--- R/common_test.R
+++ R/common_test.R
@@ -41,7 +41,7 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
                       max.call = 1e+07, max.time = NULL, taueps = 0.01, caller_function)
 {
 
-  if(!missing(functions_fname) & !is.null(functions_fname)){
+  if(!missing(functions_fname) && !is.null(functions_fname)){
     if(!file.exists(functions_fname)){
       suggested_files = list.files(path = getwd(),
                                    pattern = ifelse(caller_function == "generation", "*.cpp$", "*.R$"),
@@ -57,7 +57,7 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 
 
   if(caller_function == "generation"){
-    if(missing(net_fname) | is.null(net_fname))
+    if(missing(net_fname) || is.null(net_fname))
       return("net_fname parameter is missing! Abort")
     else{
       if(!file.exists(net_fname)){
@@ -71,20 +71,20 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 
 
   if(caller_function %in% c("sensitivity", "calibration")){
-    if((missing(reference_data) | is.null(reference_data)) & (!missing(distance_measure_fname) & !is.null(distance_measure_fname)))
+    if((missing(reference_data) || is.null(reference_data)) && (!missing(distance_measure_fname) && !is.null(distance_measure_fname)))
       return("distance_measure_fname need the reference_data parameter!")
 
   	#Maybe it's necessary to use a default distance_measure_fname.
-  	if((!missing(reference_data) & !is.null(reference_data)) & (missing(distance_measure_fname) | is.null(distance_measure_fname)))
+  	if((!missing(reference_data) && !is.null(reference_data)) && (missing(distance_measure_fname) || is.null(distance_measure_fname)))
   		return("reference_data need the distance_measure_fname parameter!")
 
-    if(!missing(reference_data) & !is.null(reference_data)){
+    if(!missing(reference_data) && !is.null(reference_data)){
       if(!file.exists(reference_data)){
         R_files = list.files(path = getwd(), pattern = "*.csv$", recursive = TRUE)
         return(paste("File",reference_data,"of reference_data parameter not exists,",
                      "list of .csv files found:\n\t",paste(unlist(R_files),collapse = "\n\t")))
       }else{
-        if(!missing(distance_measure_fname) & !is.null(distance_measure_fname)){
+        if(!missing(distance_measure_fname) && !is.null(distance_measure_fname)){
           if(!file.exists(distance_measure_fname)){
             R_files = list.files(path = getwd(), pattern = "*.R$", recursive = TRUE)
             return(paste("File", distance_measure_fname,"of distance_measure_fname parameter not exists,",
@@ -105,11 +105,11 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 
 
   if(caller_function == "sensitivity"){
-    if((missing(reference_data) | is.null(reference_data)) & (!missing(target_value_fname) & !is.null(target_value_fname)))
+    if((missing(reference_data) || is.null(reference_data)) && (!missing(target_value_fname) && !is.null(target_value_fname)))
       return("target_value_fname need the reference_data parameter!")
 
 
-    if(!missing(target_value_fname) & !is.null(target_value_fname)){
+    if(!missing(target_value_fname) && !is.null(target_value_fname)){
       if(!file.exists(target_value_fname)){
         R_files = list.files(path = getwd(), pattern = "*.R$", recursive = TRUE)
         return(paste("File", target_value_fname, "of target_value_fname parameter not exists,",
@@ -145,13 +145,13 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 
 
   if(caller_function == "calibration"){
-    if(missing(ini_v) | missing(lb_v) | missing(ub_v))
+    if(missing(ini_v) || missing(lb_v) || missing(ub_v))
       return("One or more of these parameters ini_v , lb_v or ub_v was not specified! Abort")
     else{
-      if(!is.numeric(ini_v) | !is.numeric(lb_v) | !is.numeric(ub_v))
+      if(!is.numeric(ini_v) || !is.numeric(lb_v) || !is.numeric(ub_v))
       	return("ini_v , lb_v and ub_v must be numbers")
 
-      if(length(ini_v) != length(lb_v) | length(ini_v) != length(ub_v) | length(lb_v) != length(ub_v)){
+      if(length(ini_v) != length(lb_v) || length(ini_v) != length(ub_v) || length(lb_v) != length(ub_v)){
         return("ini_v , lb_v and ub_v must have the same number of elements")
       }else{
         if(!all(ini_v > lb_v, TRUE))
@@ -172,7 +172,7 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 
 
   if(caller_function %in% c("sensitivity", "calibration", "analysis")){
-    if(missing(solver_fname) | is.null(solver_fname))
+    if(missing(solver_fname) || is.null(solver_fname))
       return("solver_fname parameter is missing! Abort")
     else{
       if(!file.exists(solver_fname)){
@@ -201,7 +201,7 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 	}
 
 	#if not specified, a runtime error is generated
-  if(!missing(parameters_fname) & !is.null(parameters_fname)){
+  if(!missing(parameters_fname) && !is.null(parameters_fname)){
     if(!file.exists(parameters_fname)){
       return(paste("File", parameters_fname, "of parameters_fname parameter does not exist!"))
     }
@@ -215,7 +215,7 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
         line = readLines(file, n=1)
         if(length(line) != 0){
           fname = unlist(strsplit(gsub(" ", "", line), ";"))[3]
-          if(!(exists(fname) | length(find(fname, numeric = TRUE)) >= 1 |
+          if(!(exists(fname) || length(find(fname, numeric = TRUE)) >= 1 ||
                !suppressWarnings(is.na(as.numeric(fname))))){
             close(file)
             return(paste(fname, "defined in", basename(parameters_fname), "does not exist! Abort"))
@@ -274,10 +274,10 @@ common_test<-function(net_fname, functions_fname = NULL, reference_data = NULL, 
 			return(paste0("The specified seed file (", seed, ") does not exist!"))
 
 		load(seed)
-		if(!exists("init_seed") | !exists("extend_seed") | !exists("n"))
+		if(!exists("init_seed") || (!(caller_function %in% c("calibration")) && !exists("extend_seed")) || (!(caller_function %in% c("calibration")) && !exists("n")))
 			return(paste0("The seed file (", seed, ") must contain three variables named init_seed, extend_seed and n!"))
 
-		if(!is.numeric(init_seed) | !is.numeric(extend_seed) | !is.numeric(n))
+		if(!is.numeric(init_seed) || (!(caller_function %in% c("calibration")) && !is.numeric(extend_seed)) || (!(caller_function %in% c("calibration")) && !is.numeric(n)))
 			return(paste0("The three variables init_seed, extend_seed and n into the seed file (", seed, ") must be a number!"))
 	}
 
