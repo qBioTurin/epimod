bef8cf249f47b3752f830071de67e757bf131741
Paolo Castagno <castagno@di.unito.it>
2021-12-01 11:57:40 +0100

    Introducing a new function

---
+++ R/mngr.worker.R
@@ -0,0 +1,121 @@
+mngr.worker <- function(id,
+												solver_fname, solver_type,taueps,
+												cmd,
+												i_time, f_time, s_time, n_run,
+												timeout, run_dir, out_fname, out_dir, seed,
+												event_times, event_function,
+												files, config = NULL,
+												parallel_processors)
+{
+	print("[analysis.worker] Starts with parameters:")
+	print(paste0("[analysis.worker] - id ", id))
+	if (!is.null(config))
+	{
+		print(paste0("[analysis.worker] - config ", config))
+		# Setup the environment
+		experiment.env_setup(id = id, files = files, config = config, dest_dir = run_dir)
+	}
+	else
+	{
+		# Handle simulations without parameters
+		dir.create(paste0(run_dir, id), recursive = TRUE, showWarnings = FALSE)
+		file.copy(from = solver_fname, to = paste0(run_dir, id))
+	}
+	print(paste0("[analysis.worker] - seed ", seed))
+	print(paste0("[analysis.worker] - params ", params))
+
+	# Change working directory to the one corresponding at the current id
+	pwd <- getwd()
+	setwd(paste0(run_dir,id))
+
+	print("[calibration.worer] Starting simulations..")
+	if(n_run != 1)
+	{
+		print("[analysis.worker] Creating subdirectories...")
+		# setup the environment for each run
+		fns <- list.files(recursive = FALSE)
+		print(paste0("[analysis.worker] ", fns))
+		lapply(X = c(1:n_run),
+					 FUN = function(X, fns){
+					 	dir.create(paste0(X))
+					 	file.copy(from = fns,
+					 						to = paste0(X, .Platform$file.sep, fns))
+					 },
+					 fns = fns)
+		print("[analysis.worker] Done creating subdirectories")
+		# Create a cluster
+		# cl <- makeCluster(parallel_processors,
+		# 									type = "FORK")
+		cl <- makeCluster(1,
+											type = "FORK")
+		# Launch simulations
+		start_time <- Sys.time()
+		parLapply(cl = cl,
+							fun = function(X, cmd, i_time, f_time, s_time, event_times, event_function, out_fname, id){
+								pwd <- getwd()
+								setwd(paste0(X))
+								print(paste0("[analysis.worker] Running simulation ", id, "-", X, "..."))
+								experiment.run(id = X,
+															 cmd = cmd,
+															 i_time = i_time,
+															 f_time = f_time,
+															 s_time = s_time,
+															 n_run = 1,
+															 seed = seed + ((id-1)*n_run+X),
+															 event_times = event_times,
+															 event_function = event_function,
+															 out_fname = paste0(out_fname,"-", id))
+								print(paste0("[analysis.worker] Simulation ", id, "-", X, " done!"))
+								setwd(pwd)
+							},
+							X = c(1:n_run),
+							id = id,
+							cmd = cmd,
+							i_time = i_time,
+							f_time = f_time,
+							s_time = s_time,
+							event_times = event_times,
+							event_function = event_function,
+							out_fname = out_fname)
+		elapsed <-  Sys.time()-start_time
+		print("[analysis.worker] Merging files..")
+		# Get file names
+		res <- list.files(pattern = ".trace",
+											recursive = TRUE)
+		# Merge all trace files in one
+		lapply(X = res, function(X, outname)
+		{
+			# tr <- read.csv(paste0(run_dir, id, .Platform$file.sep, out_fname, "-", basename(X), ".trace"), sep = "")
+			tr <- read.csv(X,
+										 sep = "")
+			if (!file.exists(outname)) {
+				write.table(tr, file = outname, sep = " ", col.names = TRUE, row.names = FALSE)
+			} else {
+				write.table(tr, file = outname, append = TRUE, sep = " ", col.names = FALSE, row.names = FALSE)
+			}
+			unlink(x = basename(dirname(X)),
+						 recursive = TRUE,
+						 force = TRUE)
+		},
+		outname = paste0(out_dir, out_fname,"-", id, ".trace"))
+		print("[analysis.worker] Done merging files")
+	} else {
+		print(paste0("[analysis.worker] Running simulation ", id, "..."))
+		elapsed <- experiment.run(id = id,
+															cmd = cmd,
+															i_time = i_time,
+															f_time = f_time,
+															s_time = s_time,
+															n_run = n_run,
+															event_times = event_times,
+															event_function = event_function,
+															out_fname = out_fname)
+		print(paste0("[analysis.worker] Simulation ", id, " done!"))
+	}
+	cat("\n\n",id,": Execution time ODEs:",elapsed, "sec.\n")
+	# Change the working directory back to the original one
+	setwd(pwd)
+	# Move relevant files to their final location and remove all the temporary files
+	experiment.env_cleanup(id = id, run_dir = run_dir, out_fname = out_fname, out_dir = out_dir)
+	return(elapsed)
+}
