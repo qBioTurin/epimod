de5c8f4a76b96388f68101c34d97aa2b62069479
Paolo Castagno <castagno@di.unito.it>
2021-12-02 09:08:19 +0100

    Substituting calibration.worker with mngr.worker

--- inst/R_scripts/calibration.mngr.R
+++ inst/R_scripts/calibration.mngr.R
@@ -44,7 +44,7 @@ objfn <- function(x, params, seed) {
 	traces_name <- file.path(params$out_dir, traces_name)
 	print(paste0("[objfn] Counter ", cnt))
 	print("[objfn] Renaming output file in ", gsub(pattern = "(-0.trace)",
-																								 replacement = paste0("-", cnt, ".trace"),
+																								 replacement = paste0("-", as.numeric(cnt), ".trace"),
 																								 x = traces_name))
 	file.rename(traces_name, gsub(pattern = "(-0.trace)",
 																replacement = paste0("-", cnt, ".trace"),
