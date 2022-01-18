e0a147e1f6ffd2f94c3cba1ac180f7da2f2dadec
Paolo Castagno <castagno@di.unito.it>
2022-01-18 12:21:07 +0100

    Bug fix

--- R/docker_run.R
+++ R/docker_run.R
@@ -7,7 +7,7 @@
 #' @examples
 #'\dontrun{
 ##'     #running runDocker
-#'      docker.run(params=NULL)
+#'      docker.run(params=NULL, changeUID=TRUE, debug=FALSE)
 #'
 #' }
 #' @export
@@ -52,34 +52,27 @@ docker.run <- function( params=NULL, changeUID=TRUE, debug=FALSE){
     cat(".\n\n")
     ## Check the Docker container exit status
     dockerExit <- system(paste("docker inspect -f {{.State.ExitCode}}",dockerid),intern= T)
-    cat("\nDocker exit status:",dockerExit,"\n\n")
-	
+
     if(as.numeric(dockerExit)!=0){
-	    system(paste("docker logs ", substr(dockerid,1,12), " &> ", substr(dockerid,1,12),"_error.log", sep=""))
+    	cat("\nExecution is interrupted\n")
 	    cat(paste("\nDocker container ", substr(dockerid,1,12), " had exit different from 0\n", sep=""))
-	    cat("\nExecution is interrupted\n")
+    	system(paste("docker logs ", substr(dockerid,1,12), " &> ", substr(dockerid,1,12),"_error.log", sep=""))
 	    cat("The container's log is saved at: ")
 	    system(paste0("docker inspect --format=","'{{.LogPath}}' ",dockerid))
 	    cat(paste("Please send to beccuti@unito.it this error: Docker failed exit 0,\n the description of the function you were using and the following error log file,\n which is saved in your working folder:\n", substr(dockerid,1,12),"_error.log\n", sep=""))
 	    system("echo 3 > ExitStatusFile 2>&1")
 	    return(3)
     }else{
+    	cat("\nDocker exit status:",dockerExit,"\n\n")
     	file.remove("dockerID")
     	system(paste("docker rm -f ",dockerid),intern= T)
-	if(debug==TRUE){
-	    system(paste("docker logs ", substr(dockerid,1,12), " &> ", substr(dockerid,1,12),".log", sep=""))
-	    cat("The container's log is saved at: ")
-	    system(paste0("docker inspect --format=","'{{.LogPath}}' ",dockerid))
+    	if(debug==TRUE){
+    		system(paste("docker logs ", substr(dockerid,1,12), " &> ", substr(dockerid,1,12),".log", sep=""))
+    		cat("The container's log is saved at: ")
+    		system(paste0("docker inspect --format=","'{{.LogPath}}' ",dockerid))
     	}
+    	#Normal Docker execution
     	system("echo 0 > ExitStatusFile 2>&1")
     	return(0)
     }
-    
-
-    file.remove("dockerID")
-    system(paste("docker rm -f ",dockerid),intern= T)
-
-    #Normal Docker execution
-    system("echo 0 > ExitStatusFile 2>&1")
-    return(0)
 }
