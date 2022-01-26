97d58bcfa9f1cd07dbf01e736a5543fccb843967
Paolo Castagno <castagno@di.unito.it>
2022-01-26 09:23:48 +0100

    Fixing seed management

--- inst/R_scripts/sensitivity.mngr.R
+++ inst/R_scripts/sensitivity.mngr.R
@@ -88,30 +88,30 @@ cl <- makeCluster(config_processors,
 # Save session's info
 clusterEvalQ(cl, sessionInfo())
 
-parLapply( cl = cl,
-					 X = c(1:params$n_config),
-					 fun = function(X, params, seed, parallel_processors)
-					 {
+parLapply(cl = cl,
+					X = c(1:params$n_config),
+					fun = function(id, params, seed, parallel_processors)
+					{
 					 	if(length(params$event_times) != 0)
 					 	{
-					 		i_s = seed + (X - 1)*(params$n_run * length(params$event_times))
+					 		i_s = seed + (id - 1)*length(params$event_times)
 					 	} else {
-					 		i_s = seed + (X - 1)*params$n_run
+					 		i_s = seed + (id - 1)
 					 	}
-					 	mngr.worker(id = X, solver_fname = params$files$solver_fname,
+					 	mngr.worker(id = id, solver_fname = params$files$solver_fname,
 					 							solver_type = params$solver_type, taueps = params$taueps,
 					 							i_time = params$i_time, f_time = params$f_time,
 					 							s_time = params$s_time, n_run = 1,
 					 							timeout = params$timeout, run_dir = params$run_dir,
-					 							out_fname = params$out_fname,out_dir = params$out_dir,
+					 							out_fname = params$out_fname, out_dir = params$out_dir,
 					 							seed = i_s, event_times = params$event_times,
 					 							event_function = params$event_function,
 					 							files = params$files, config = params$config,
 					 							parallel_processors = parallel_processors)
-					 },
-					 params = params,
-					 seed = init_seed,
-					 parallel_processors = run_processors)
+					},
+					params = params,
+					seed = init_seed,
+					parallel_processors = run_processors)
 # Print all the output to the stdout
 # system(paste0("cat ", params$out_fname,".log >&2"))
 # unlink(x = paste0(params$out_fname,".log"), force = TRUE)
