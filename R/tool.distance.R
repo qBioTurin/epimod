a8e8ac9263ee1c0aed399efb4e0258ce06f1082b
Paolo Castagno <castagno@di.unito.it>
2022-01-26 11:25:14 +0100

    Parameter function_fname

--- R/tool.distance.R
+++ R/tool.distance.R
@@ -4,7 +4,7 @@ tool.distance <- function(id,
 													# distance_measure_fname,
 													distance_measure,
 													reference_data,
-													functions_fname){
+													function_fname){
 	print("[tool.distance] Begin computing distance")
 	pwd <- getwd()
 	setwd(out_dir)
@@ -12,13 +12,17 @@ tool.distance <- function(id,
 	# Read the output and compute the distance from reference data
 	trace <- read.csv(id,
 										sep = "")
-	# Load distance definition
-	# source(distance_measure_fname)
-	source(functions_fname)
+	print(paste0("[tool.distance] Loading reference data ", reference_data))
 	# Load reference data (IMPORTANT it has to be a column vector)
 	reference <- as.data.frame(t(read.csv(reference_data, header = FALSE, sep = "")))
+	# Load distance definition
+	# source(distance_measure_fname)
+	print("tool.distance] Loading function definitions")
+	source(function_fname)
+	print("[tool.distance] CallingComputing distance")
 	# Compute the user defined distance measure
 	measure <- do.call(distance_measure, list(reference, trace))
 	setwd(pwd)
+	print("[tool.distance] Done computing distance")
 	return(data.frame(measure=measure, id=id))
 }
