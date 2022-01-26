6208e79dcb3cca20c1f6b8d5aeedb3b9eb03e0b5
Paolo Castagno <castagno@di.unito.it>
2022-01-26 11:25:11 +0100

    Parameter function_fname

--- R/tool.distance.R
+++ R/tool.distance.R
@@ -1,20 +1,20 @@
 # Function to compute the distance between one simulation trace and the reference data
 tool.distance <- function(id,
-																 ## run_dir,
-																 ## out_fname,
-																 out_dir,
-																 distance_measure_fname,
-																 distance_measure,
-																 reference_data){
+													out_dir,
+													# distance_measure_fname,
+													distance_measure,
+													reference_data,
+													functions_fname){
+	print("[tool.distance] Begin computing distance")
 	pwd <- getwd()
 	setwd(out_dir)
-	system(paste0("echo \"Trying to open ", id, "\""))
+	print(paste0("[tool.distance] opening file ", out_dir, .Platform$path.sep, id))
 	# Read the output and compute the distance from reference data
-	## trace <- read.csv(paste0(out_fname, "-", id, "-1-1.trace"), sep = "")
 	trace <- read.csv(id,
 										sep = "")
 	# Load distance definition
-	source(distance_measure_fname)
+	# source(distance_measure_fname)
+	source(functions_fname)
 	# Load reference data (IMPORTANT it has to be a column vector)
 	reference <- as.data.frame(t(read.csv(reference_data, header = FALSE, sep = "")))
 	# Compute the user defined distance measure
