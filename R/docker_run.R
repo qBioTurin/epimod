a17ddb0fab5df8a2744c6d958a027ba08026c1fe
qBioTurin <37082085+qBioTurin@users.noreply.github.com>
2023-03-25 10:13:55 +0100

    Update docker_run.R
    Fixing authorization in Docker

--- R/docker_run.R
+++ R/docker_run.R
@@ -31,11 +31,11 @@ docker.run <- function( params=NULL, changeUID=TRUE, debug=FALSE){
     {
         userid=system("id -u", intern = TRUE)
         groupid=system("id -g", intern = TRUE)
-        cat(paste("docker run --privileged=true  --user=",userid,":",groupid," ",params,"\n\n", sep=""))
-        system(paste("docker run --privileged=true  --user=",userid,":",groupid," ",params, sep=""))
+        cat(paste("docker run   --user=",userid,":",groupid," ",params,"\n\n", sep=""))
+        system(paste("docker run   --user=",userid,":",groupid," ",params, sep=""))
     } else {
-        cat(paste("docker run --privileged=true ",params,"\n\n", sep=""))
-    		system(paste("docker run --privileged=true ",params, sep=""))
+        cat(paste("docker run  ",params,"\n\n", sep=""))
+    		system(paste("docker run  ",params, sep=""))
     }
 
     ## Get the Docker ID from file
