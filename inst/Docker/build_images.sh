81ce631ac9c64f93615f3b2d5d292089621813b4
Mandringo <mandringo.duregon@gmail.com>
2025-01-10 15:10:55 +0100

    build_images modifications.

--- inst/Docker/build_images.sh
+++ inst/Docker/build_images.sh
@@ -3,49 +3,60 @@ IMAGES=$(ls -d */)
 BASEDIR=$(pwd)
 
 # Verifica parametri
-if [ $# -eq 0 ] || [ $# -ge 4 ]; then
+if [ $# -lt 1 ] || [ $# -gt 3 ]; then
   echo "Illegal number of parameters"
   echo "Usage:"
-  echo "\tbuild_images TAGNAME IMAGE [BRANCH]"
+  echo "\tbuild_images TAGNAME [IMAGE] [BRANCH]"
   echo "where TAGNAME will be used to tag the images uploaded to Docker Hub."
   exit 1
 else
   TAG=$1
 fi
 
-if [ $# -eq 2 ]; then
+if [ $# -ge 2 ]; then
   IMAGES=($2)
 fi
 
 BRANCH=${3:-main}  # Default branch
 
+echo "Building Docker images for branch: $BRANCH"
+
+# Checkout del branch specifico
+git checkout $BRANCH || { echo "Failed to checkout branch $BRANCH"; exit 1; }
+
 for I in ${IMAGES[@]}; do
   IMG=$(echo $I | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]')
   DIR=$BASEDIR/$I
-  cd $DIR
-  if [ -f $DIR/Dockerfile ]; then
-    date > marker
-    echo "Building $IMG for branch $BRANCH"
-    if [[ $IMG == "Generation" ]]; then
-      bash $DIR/do.sh build
-    else
-      echo -e "\tExecuting docker build -t qbioturin/epimod-$IMG:$TAG-$BRANCH ."
-      docker build -t qbioturin/epimod-$IMG:$TAG-$BRANCH .
+  if [ -d "$DIR" ]; then
+    cd $DIR
+    if [ -f Dockerfile ]; then
+      date > marker
+      echo "Building $IMG for branch $BRANCH"
+      if [[ $IMG == "generation" ]]; then
+        bash $DIR/do.sh build
+      else
+        echo -e "\tExecuting docker build -t qbioturin/epimod-$IMG:$TAG-$BRANCH ."
+        docker build -t qbioturin/epimod-$IMG:$TAG-$BRANCH .
+        if [[ $? -ne 0 ]]; then
+          echo "Failed to build $IMG"
+          exit 1
+        fi
+      fi
+      rm marker
+      echo "Uploading $IMG"
+      echo -e "\tExecuting docker push qbioturin/epimod-$IMG:$TAG-$BRANCH"
+      docker push qbioturin/epimod-$IMG:$TAG-$BRANCH
       if [[ $? -ne 0 ]]; then
+        echo "Failed to push $IMG"
         exit 1
       fi
+    else
+      echo "Dockerfile missing for image qbioturin/epimod-$IMG"
     fi
-    rm marker
-    echo "Uploading $IMG"
-    echo -e "\tExecuting docker push qbioturin/epimod-$IMG:$TAG-$BRANCH"
-    docker push qbioturin/epimod-$IMG:$TAG-$BRANCH
-    if [[ $? -ne 0 ]]; then
-      exit 1
-    fi
+    cd $BASEDIR
   else
-    echo "Skipping $I: Dockerfile missing in $DIR"
+    echo "Skipping $I: Directory $DIR does not exist"
   fi
-  cd $BASEDIR
 done
 exit 0
 
